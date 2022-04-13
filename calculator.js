6136f11968101ac4d35079d44d23582ff925f61e
Jose <jnscsilvacastellon@gmail.com>
2022-04-12 23:25:43 -0700

    I was able to output the digits in the display of calculator

--- calculator.js
+++ calculator.js
@@ -1,16 +1,23 @@
-let display = document.querySelector('input[type=text]');
+// let display = document.querySelector('input[type="text"]');
+let display = document.querySelector('.display-op');
 let digits = document.querySelectorAll('.digit');
 
-
 digits.forEach(element => element.addEventListener("click", () => {
-    console.log(element.value);
+
+    let leftOp = 0;
+    let rightOp = 0;
+    if(!isNaN) { 
+        return display.value += parseInt(element.textContent);
+    } else {
+    return display.value += element.textContent;
+    }
+    
 }));
 
+// digits.forEach((element) => element.addEventListener("click", function(){
+//     element.textContent = display.value;
+// }));
 
 
 
-
-digits.forEach((element) => element.addEventListener("click", function(){
-    display.innerHTML = element.value;
-}));
 
